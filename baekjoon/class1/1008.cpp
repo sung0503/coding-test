a87971e9c6f8ac3abc8180310b63305e13b1cc01
sungch <chseung@postech.ac.kr>
2024-07-16 21:14:07 +0900

    solve 1008

---
+++ baekjoon/class1/1008.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    double a, b;
+    cin >> a >> b;
+    cout.precision(12);
+    cout << a / b << endl;
+    return 0;
+}
